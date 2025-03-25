a54122d82adfbbdc4881384d276676d39208803d
Rahul <rtabraha@ualberta.ca>
2025-03-25 00:46:07 -0600

    add event_service and testing

---
+++ student_app/test/utils/event_service_test.dart
@@ -0,0 +1,45 @@
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:student_app/utils/event_service.dart';
+
+void main() {
+  group('Event Collection Tests', () {
+    late EventService eventService;
+    late FakeFirebaseFirestore fakeFirestore;
+
+    setUp(() {
+      fakeFirestore = FakeFirebaseFirestore();
+      eventService = EventService(firestore: fakeFirestore);
+
+      fakeFirestore.collection('events').add({
+        'name': 'Test Event 1',
+        'date': DateTime(2025, 3, 15),
+        'location': 'Edmonton',
+      });
+
+      fakeFirestore.collection('events').add({
+        'name': 'Test Event 2',
+        'date': DateTime(2025, 3, 16),
+        'location': 'Calgary',
+      });
+    });
+
+    test('gets all events', () async {
+      final events = await eventService.getAllEvents();
+      expect(events.length, 2);
+    });
+
+    test('gets events for a specific date', () async {
+      final date = DateTime(2025, 3, 15);
+      final events = await eventService.getEventsByDate(date);
+      expect(events.length, 1);
+      expect(events.first['name'], 'Test Event 1');
+    });
+
+    test('gets events for a specific location', () async {
+      final events = await eventService.getEventsByLocation('Calgary');
+      expect(events.length, 1);
+      expect(events.first['location'], 'Calgary');
+    });
+  });
+}
